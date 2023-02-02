e4d1c6a4dd679ed7635e37dd0bdc124f5bd54c56
rdrgDaniel <107716819+rdrgDaniel@users.noreply.github.com>
2023-02-02 17:19:15 -0600

    Borré la carpeta de Eliminar
    Borré la carpeta de Eliminar para subir una nueva con los enlaces corregidos

--- 6to/Funciones/eliminar/eliminar.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("location:Basededatos.php");
-include "conexion.php";
-$id=$_GET["id"];
-    $sql=$conexion->query("DELETE FROM ds WHERE ID='$id'");
-?>
--- 6to/Funciones/eliminar/eliminarsemestre.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "conexion.php";
-$id=$_GET["id"];
-echo $id;
-    $sql=$conexion->query("DELETE FROM ds WHERE ID='$id'");
- if ($sql==1)
-    header("location:graficarsemestre.php");
-?>
-<input type="text" name="id" value="<?=$_GET["id"]?>">
