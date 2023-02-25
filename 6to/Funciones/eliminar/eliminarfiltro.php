d0a7bc84ec0df8bf2d4f028ef239f757dd23cc08
Diegogranja3 <107713780+Diegogranja3@users.noreply.github.com>
2023-02-25 17:43:48 -0600

    Eliminarfiltro.php comentado

--- 6to/Funciones/eliminar/eliminarfiltro.php
+++ 6to/Funciones/eliminar/eliminarfiltro.php
@@ -1,8 +1,13 @@
 <?php
+///Se conecta a la base
 include ("../../Base-de-datos/conexion.php");
+///Recolecta un ID
 $id=$_GET["id"];
+///Imprime el ID
 echo $id;
+///Borra el registro que contenga ese ID
     $sql=$conexion->query("DELETE FROM ds WHERE ID='$id'");
+///Si se borra entonces nos regresa a los filtros
  if ($sql==1)
     header("location:../filtros/filtrofechas.php");
 ?>
