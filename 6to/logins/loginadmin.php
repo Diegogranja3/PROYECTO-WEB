e9ff42e04bb66a1d5a7d5bc9d0025c9654fe5a56
Diegogranja3 <107713780+Diegogranja3@users.noreply.github.com>
2023-03-01 20:10:59 -0600

    Update loginadmin.php

--- 6to/logins/loginadmin.php
+++ 6to/logins/loginadmin.php
@@ -1,142 +1,102 @@
+<script>
+date = new Date();
+year = date.getFullYear();
+month = date.getMonth() + 1;
+day = date.getDate();
+document.getElementById("current_date").innerHTML = month + "/" + day + "/" + year;
+</script>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+    <!-- Required meta tags-->
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Materiales</title>
-    <link rel="icon" type="image/png" href="images/aa.png"/>
-    <link href="C:\xampp\htdocs\xanpon\estilo.css" rel="stylesheet" type="text/css" media="all">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Colorlib Templates">
+    <meta name="author" content="Colorlib">
+    <meta name="keywords" content="Colorlib Templates">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-   <style>
-  body{
-background: rgb(0,212,255);
-background: -moz-linear-gradient(90deg, rgba(0,212,255,0) 0%, rgba(1,148,130,0) 0%, rgba(134,1,1,0.5046393557422969) 0%, rgba(0,60,222,0) 49%, rgba(1,144,122,1) 100%, rgba(1,132,98,0) 100%);
-background: -webkit-linear-gradient(90deg, rgba(0,212,255,0) 0%, rgba(1,148,130,0) 0%, rgba(134,1,1,0.5046393557422969) 0%, rgba(0,60,222,0) 49%, rgba(1,144,122,1) 100%, rgba(1,132,98,0) 100%);
-background: linear-gradient(90deg, rgba(0,212,255,0) 0%, rgba(1,148,130,0) 0%, rgba(134,1,1,0.5046393557422969) 0%, rgba(0,60,222,0) 49%, rgba(1,144,122,1) 100%, rgba(1,132,98,0) 100%);
-filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#00d4ff",endColorstr="#018462",GradientType=1);
-      font-family: "century ghotic";
-    }
-.input-container {
-    display: -ms-flexbox; /* IE10 */
-    display: flex;
-    width: 100%;
-    margin-bottom: 15px;
+    <!-- Title Page-->
+    <title>Login de operador</title>
+
+    <!-- Icons font CSS-->
+    <link href="../vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="../vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Vendor CSS-->
+    <link href="../vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="../vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="../css/main.css" rel="stylesheet" media="all">
+</head>
+<style>
+body{
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.9)), url("https://media.discordapp.net/attachments/1068260110415179890/1080292960228352141/932271.jpg?width=788&height=513");
 }
-
-.icon {
-    padding: 10px;
-    background: dodgerblue;
-    color: white;
-    min-width: 50px;
-    text-align: center;
-}
-
-.input-field {
-    width: 100%;
-    padding: 10px;
-    outline: none;
-}
-
-    .input-field:focus {
-        border: 2px solid dodgerblue;
-    }
-.btn {
-    background-color: dodgerblue;
-    color: white;
-    padding: 15px 20px;
-    border: none;
-    cursor: pointer;
-    width: 100%;
-    opacity: 0.9;
-}
-
-    .btn:hover {
-        opacity: 1;
-    }
-
-    .mayor{
-        margin:10%;
-        border-radius:25px;
-    background-color: #f2f2f2;
-    padding: 20px;
-       
-    }
-
-    input[type=text], select {
-    width: 100%;
-    padding: 12px 20px;
-    margin: 8px 0;
-    display: inline-block;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    box-sizing: border-box;
-    box-shadow: 10px 10px 10px #BDBFD5;
-  }
-
-  input[type=text]:focus {
-  background-color:   #cce6ff;
-  } 
-
-  input[type=password], select {
-    width: 100%;
-    padding: 12px 20px;
-    margin: 8px 0;
-    display: inline-block;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    box-sizing: border-box;
-    box-shadow: 10px 10px 10px #BDBFD5;
-  }
-
-  input[type=password]:focus {
-  background-color:   #cce6ff;
-  } 
-
- input[type=button], select {
-    width: 100%;
-    padding: 12px 20px;
-    margin: 8px 0;
-    display: inline-block;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    box-sizing: border-box;
-    box-shadow: 10px 10px 10px #BDBFD5;
-  }
-
-  input[type=button]:focus {
-  background-color:   #cce6ff;
-  } 
-
-  h2{
-     font-family: "Verdana";
-  }
-
-   </style>
-  </head>
-
+    </style>
 <body>
- <div class="mayor">
-    <form  style="max-width:500px;margin:auto" id="formulario" name="formulario" method="POST">
-        <?php
+    <div class="page-wrapper  p-t-130 p-b-100 font-poppins">
+        <div class="wrapper wrapper--w680">
+            <div class="card card-4">
+                <div class="card-body">
+                    <h2 class="title">Inicio de sesión</h2>
+                    <form method="POST" id="formulario" name="formulario">
+                    <?php
                 include("../Base-de-datos/conexion.php"); 
-                 include("../ciberseguridad/sesiones/controladmin.php"); 
+                include("../ciberseguridad/sesiones/controladmin.php"); 
                 ?>
-  <CENTER><h2><b>Login de administrador</b></h2></CENTER>
-  <div class="input-container">
-    <input class="input-field" type="text" placeholder="Usuario" name="usuario">
-  </div>
-  
-  <div class="input-container">
-    <input class="input-field" type="password" placeholder="Clave" name="clave">
-  </div>
-
-        <input type="submit" class="btn" name="Continuar" value="Continuar">
-        <br>        <br>
-    <a href="../index.html" class="btn btn-danger">Regresar</a>
-</form>
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Usuario</label>
+                                    <input placeholder="Usuario" name="usuario" class="input--style-4" type="text" name="first_name">
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Clave</label>
+                                    <input placeholder="Clave" name="clave" class="input--style-4" type="password" name="last_name">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Fecha</label>
+                                    <div class="input-group-icon">
+                                        <input name="fecha"  readonly><script>date = new Date().toLocaleDateString();document.write(date);</script>
+                                        <i class="zmdi zmdi-calendar-note input-icon js-btn-calendar"></i>
+                                    </div>
+                                </div>
+</div>
+                        </div>
+                        <div class="row row-space">
+                        <div class="p-t-15">
+                        <button class="btn btn--radius-2 btn--blue" type="submit"name="Continuar" value="Continuar">Continuar</button>
+                        <a href="../index.html" class="btn btn-danger">Regresar</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-  </h2>
-</body>
-</html>
 
+    <!-- Jquery JS-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <!-- Vendor JS-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <script src="vendor/datepicker/moment.min.js"></script>
+    <script src="vendor/datepicker/daterangepicker.js"></script>
+
+    <!-- Main JS-->
+    <script src="js/global.js"></script>
+
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+
+</html>
+<!-- end document-->
