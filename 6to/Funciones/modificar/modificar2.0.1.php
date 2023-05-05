81813a7b05479a83bd81e9c1dd583983281026d1
rdrgDaniel <107716819+rdrgDaniel@users.noreply.github.com>
2023-05-04 19:49:25 -0600

    Código revampeado
    Se actualizó el código para que se adaptara a la nueva base de datos y para que tenga un diseño apropiado

--- 6to/Funciones/modificar/modificar2.0.1.php
+++ 6to/Funciones/modificar/modificar2.0.1.php
@@ -28,9 +28,9 @@ $sql=$conexion->query("SELECT*FROM ds WHERE ID=$id");
 </head>
 <style>
     body{
-        background-image:linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.9)),url('https://cdn.discordapp.com/attachments/1068260110415179890/1080668816465481758/a.png');
-        background-repeat: no-repeat;
-        background-size: cover;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.9)), url("../../images/loginfondowo.png");
     }
     img{
         max-width: 100%;
@@ -44,7 +44,7 @@ $sql=$conexion->query("SELECT*FROM ds WHERE ID=$id");
     </style>
 <body>
 <form class="formulario" method="POST">
-<div class="centroform"><center><h3 class="display-6" style="color: blanchedalmond;">Registro rapido</h3></center></div>
+<div class="centroform"><center><h3 class="display-6" style="color: blanchedalmond;">Modificación rapida</h3></center></div>
 <div class="page-wrapper  p-t-130 p-b-100 font-poppins">
         <div class="wrapper wrapper--w680">
             <div class="card card-4">
@@ -55,56 +55,49 @@ $sql=$conexion->query("SELECT*FROM ds WHERE ID=$id");
             include "modificarfiltro.php";
             while($datos=$sql->fetch_object()){?>
             <div class="mb-3">
- <label for="exempleInputEmail1" class="form-label m-auto">Nombre de la persona</label>
+            <label for="exempleInputEmail1" class="form-label m-auto">Nombre de la persona</label>
                 <input required type="text" class="form-control" name="nombre" value="<?=$datos->Nombre_completo?>">
-<div class="mb-3">
+            </div>
+                <div class="col-2">               
                 <label for="exempleInputEmail1" class="form-label m-auto">Papel(KG)</label>
                 <input required type="number" class="form-control" name="papel" value="<?=$datos->Papel_KG?>">
-</div>
-<div class="mb-3">
+            </div>            
+<div class="col-2">                
                 <label for="exempleInputEmail1" class="form-label m-auto">Carton(KG)</label>
                 <input required type="number" class="form-control" name="carton" value="<?=$datos->Carton_KG?>">
-</div>
-<div class="mb-3">
+</div><div><br></div>
+<div class="col-2">               
                 <label for="exempleInputEmail1" class="form-label m-auto">Vidrio(KG)</label>
                 <input required type="number" class="form-control" name="vidrio" value="<?=$datos->Vidrio_KG?>">
-</div>
-<div class="mb-3">
+            </div>
+<div class="col-2">                
                 <label for="exempleInputEmail1" class="form-label m-auto">PET(KG)</label>
                 <input required type="number" class="form-control" name="pet" value="<?=$datos->PET_KG?>">
-</div>
-<div class="mb-3">
+            </div><div><br></div>
+            <div class="col-2">               
                 <label for="exempleInputEmail1" class="form-label m-auto">PEAD(KG)</label>
                 <input required type="number" class="form-control" name="pead" value="<?=$datos->PEAD_KG?>">
+            </div>
+         <div class="col-2">                
+                <label for="exempleInputEmail1" class="form-label m-auto">Electonicos(KG)</label>
+                <input required type="number" class="form-control" name="elec" value="<?=$datos->Electronicos_KG?>">
+            </div><div><br></div>
+            <div class="col-2">
+                <label for="exempleInputEmail1" class="form-label m-auto">Toner(piezas)</label>
+                <input required type="number" class="form-control" name="cton" value="<?=$datos->Cartuchos_toner?>">
 </div>
-<div class="mb-3">
-                <label for="exempleInputEmail1" class="form-label m-auto">PEBD(KG)</label>
-                <input required type="number" class="form-control" name="pebd" value="<?=$datos->PEBD_KG?>">
-</div>
-<div class="mb-3">
-                <label for="exempleInputEmail1" class="form-label m-auto">BOPP(KG)</label>
-                <input required type="number" class="form-control" name="bopp" value="<?=$datos->BOPP_KG?>">
-</div>
-<div class="mb-3">
-                <label for="exempleInputEmail1" class="form-label m-auto">Aluminio(KG)</label>
-                <input required type="number" class="form-control" name="aluminio" value="<?=$datos->Aluminio_KG?>">
-</div>
-<div class="mb-3">
-                <label for="exempleInputEmail1" class="form-label m-auto">Hojalata(KG)</label>
-                <input required type="number" class="form-control" name="hojalata" value="<?=$datos->Hojalata_KG?>">
-</div>
-<div class="mb-3">
-                <label for="exempleInputEmail1" class="form-label m-auto">Baterias(KG)</label>
-                <input required type="number" class="form-control" name="baterias" value="<?=$datos->Baterias_KG?>">
-</div>
-<div class="mb-3">
-                <label for="exempleInputEmail1" class="form-label m-auto">Madera(KG)</label>
-                <input required type="number" class="form-control" name="madera" value="<?=$datos->Madera_KG?>">
-</div>
-<div class="mb-3">
-                <label for="exempleInputEmail1" class="form-label m-auto">Electronicos(KG)</label>
-                <input required type="number" class="form-control" name="electronicos" value="<?=$datos->Electronicos_KG?>">
-</div>
+<div class="col-2">
+                <label for="exempleInputEmail1" class="form-label m-auto">Latas(KG)</label>
+                <input required type="number" class="form-control" name="lat" value="<?=$datos->Latas_KG?>">
+</div><div><br></div>
+<div class="col-2">
+                <label for="exempleInputEmail1" class="form-label m-auto">Taparroscas(KG)</label>
+                <input required type="number" class="form-control" name="tapa" value="<?=$datos->Taparroscas_KG?>">
+            </div>
+            <div class="col-2">
+                <label for="exempleInputEmail1" class="form-label m-auto">Residuos(KG)</label>
+                <input required type="number" class="form-control" name="resp" value="<?=$datos->Residuos_peligrosos_KG?>">
+            </div><div><br></div>
 <div class="mb-3">
                 <label for="exempleInputEmail1" class="form-label m-auto">Fecha</label>
                 <input required type="date" class="form-control" name="fecha" value="<?=$datos->Fecha_entrega?>">
@@ -113,8 +106,8 @@ $sql=$conexion->query("SELECT*FROM ds WHERE ID=$id");
             ?>
 
                
-<button type="submit" class="btn btn-primary" name="btnmod" value="ok">Modificar registro</button>
-<a href='../../Funciones/filtros/filtrofechas.php' class='btn btn-danger'>Regresar</a>
+<div class="col-2"><div class="input-group"><button type="submit" class="btn btn-primary" name="btnmod" value="ok">Modificar</button></div></div>
+<div class="col-2"><div class="input-group"><a href='../../Funciones/filtros/filtrofechas.php' class='btn btn-danger'>Regresar</a></div></div>
 </form>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </div>
